6c2ae13ba1400fc9a6ae81fae8995cc5f0e14466
Thomas Duesing <TFDuesing@gmail.com>
2013-11-18 16:01:03 -0600

    prevent udev from renaming eth0
    Turns out, the version of systemd/udev that ships with Fedora 20 will
    rename eth0 to something like “enp0s3” unless you perform the right
    incantations.

--- common/scripts/network.sh
+++ common/scripts/network.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-# Remove MAC addresse and UUID from non-loopback interface configuration files
-sed --in-place=.original '/^HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-eth*
-sed --in-place=.original '/^UUID/d' /etc/sysconfig/network-scripts/ifcfg-eth*
+# Remove MAC address and UUID from non-loopback interface configuration files
+sed --in-place '/^HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-eth*
+sed --in-place '/^UUID/d' /etc/sysconfig/network-scripts/ifcfg-eth*
+
+# Tell udev to disable the assignment of fixed network interface names
+# http://www.freedesktop.org/wiki/Software/systemd/PredictableNetworkInterfaceNames/
+ln -s /dev/null /etc/udev/rules.d/80-net-name-slot.rules
