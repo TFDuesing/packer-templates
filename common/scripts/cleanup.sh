bed07be63e9c07f7e6786344d4c6d0d8ed81b36c
Thomas Duesing <TFDuesing@gmail.com>
2013-12-27 22:35:31 -0600

    fix incorrect SELinux context labels

--- common/scripts/cleanup.sh
+++ common/scripts/cleanup.sh
@@ -11,6 +11,9 @@ rm --recursive --force /var/lib/yum/history/*
 yum history new
 truncate --no-create --size=0 /var/log/yum.log
 
+# Change any incorrect SELinux context labels
+fixfiles -R -a restore
+
 # Force the filesystem to reclaim space from deleted files
 dd if=/dev/zero of=/var/tmp/zeros bs=1M
 rm --force /var/tmp/zeros
