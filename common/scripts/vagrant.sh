721942a3faf95e860907102244117829685854fe
Thomas Duesing <TFDuesing@gmail.com>
2013-12-27 22:34:34 -0600

    fix SELinux context for ~/.ssh directory

--- common/scripts/vagrant.sh
+++ common/scripts/vagrant.sh
@@ -25,7 +25,7 @@ mv vagrant.pub /home/vagrant/.ssh/authorized_keys
 chown --recursive vagrant:vagrant /home/vagrant/.ssh/
 chmod 0700 /home/vagrant/.ssh/
 chmod 0600 /home/vagrant/.ssh/authorized_keys
-chcon --recursive unconfined_u:object_r:user_home_t:s0 /home/vagrant/.ssh/
+chcon --recursive unconfined_u:object_r:ssh_home_t:s0 /home/vagrant/.ssh/
 
 # Delete and lock root password
 passwd --delete root
