16dd2795fb86d94c3ce07c28de0e1103d17c15f5
Thomas Duesing <TFDuesing@gmail.com>
2015-07-29 18:05:54 -0500

    add Fedora 22 template
    The Vagrant base box produced from this template will be a bit larger
    than the Fedora 21 box due to (what appears to be) a limitation of dnf
    that prevents “mandatory” packages in a group from being removed at
    install time.

---
+++ Fedora-22/README.markdown
@@ -0,0 +1,8 @@
+Fedora-22
+=========
+
+This Packer template can be used to build minimal Fedora 22 base boxes for Vagrant’s VirtualBox and VMware providers, starting from Fedora 22 Server’s netinstall ISO for x86_64 architectures.
+
+Vagrant base boxes built from this template can be downloaded from [Atlas][1] directly, or by running `vagrant init TFDuesing/Fedora-22`.
+
+[1]:https://atlas.hashicorp.com/TFDuesing/boxes/Fedora-22
---
+++ Fedora-22/scripts/30cleanup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# Remove UUID from non-loopback interface configuration files
+sed --in-place '/^UUID/d' /etc/sysconfig/network-scripts/ifcfg-eth*
+
+# Remove random-seed, so it’s not the same in every image
+rm --force /var/lib/systemd/random-seed
+
+# Truncate machine-id, for the same reasons random-seed is removed
+# Note: this image will not boot if /etc/machine-id is not present, but systemd
+# will generate a new machine ID if /etc/machine-id is present but empty
+truncate --size=0 /etc/machine-id
+
+# Clean up old dnf repo data & logs
+dnf --assumeyes autoremove
+dnf clean all
+rm --recursive --force /var/lib/dnf/history/*
+rm --recursive --force /var/lib/dnf/yumdb/*
+truncate --no-create --size=0 /var/log/dnf.*
+
+# Force the filesystem to reclaim space from deleted files
+dd if=/dev/zero of=/var/tmp/zeros bs=1M
+rm --force /var/tmp/zeros
